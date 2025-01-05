8df135809efbf0a8ede493acaddd2033f14fb86b
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2025-01-05 12:20:24 +0400

    feat: Implement soultion for carbon dating.

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,20 +6,35 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string") {
+    return false;
+  }
+  let activity = Number.parseFloat(sampleActivity);
+  if (
+    typeof activity != "number" ||
+    isNaN(activity) ||
+    activity > MODERN_ACTIVITY ||
+    activity <= 0
+  ) {
+    return false;
+  }
+  let date = Math.ceil(
+    (Math.log(MODERN_ACTIVITY / activity) * HALF_LIFE_PERIOD) / 0.693
+  );
+
+  return date;
 }
 
 module.exports = {
