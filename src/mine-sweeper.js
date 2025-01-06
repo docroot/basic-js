c27c1abf91c8f550eaf8b457ea55d500438a4c18
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2025-01-06 17:14:02 +0400

    feat: Implement soultion for mine sweeper.

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const field = Array.from({ length: rows }, () => Array(cols).fill(0));
+
+  const directions = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (matrix[y][x] === true) {
+        for (let [dy, dx] of directions) {
+          const y1 = y + dy;
+          const x1 = x + dx;
+          if (y1 >= 0 && y1 < rows && x1 >= 0 && x1 < cols) {
+            field[y1][x1]++;
+          }
+        }
+      }
+    }
+  }
+
+  return field;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
