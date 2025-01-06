35f7942471718c916c7d5f499bfe28bc1ca8ad51
Dmitry Kostikov <dmitry.kostikov@gmail.com>
2025-01-06 15:39:59 +0400

    feat: Implement soultion for common char count.

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const charMap1 = new Map();
+  for (const char of s1) {
+    charMap1.set(char, (charMap1.get(char) || 0) + 1);
+  }
+  const charMap2 = new Map();
+  for (const char of s2) {
+    charMap2.set(char, (charMap2.get(char) || 0) + 1);
+  }
+  res = 0;
+  for (const key of charMap1.keys()) {
+    if (charMap2.get(key)) {
+      const c1 = charMap1.get(key);
+      const c2 = charMap2.get(key);
+      res += c2 > c1 ? c1 : c2;
+    }
+  }
+  return res;
 }
 
 module.exports = {
